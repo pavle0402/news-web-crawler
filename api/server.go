e24126d3f6963ee6cc794cc3bfca778fffda1bc4
Pavle Stojicevic <pavles2002@gmail.com>
2025-05-23 09:38:53 +0200

    router added

--- api/server.go
+++ api/server.go
@@ -1,16 +1,20 @@
 package api
 
 import (
+	"crawler/handlers"
 	"log"
 	"net/http"
-	"crawler/handlers"
-)
 
+	"github.com/go-chi/chi/v5"
+)
 
 func StartServer() {
 	log.Println("Starting server on :8080")
 
-	http.HandleFunc("/api/start-crawler", handlers.CrawlerHandler)
+	r := chi.NewRouter()
+
+	r.Post("/api/start-crawler", handlers.CrawlerHandler)
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
